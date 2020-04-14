e2fb04c7d7b063fdcd13d105657b69e3ee32fd48
Eszter Gabor <mrxaxen@gmail.com>
2020-04-14 14:56:33 +0200

    Initial commit provided by spring boot initializer

---
+++ src/main/java/hu/elte/finances/FinancesApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApplication.class, args);
+	}
+
+}
