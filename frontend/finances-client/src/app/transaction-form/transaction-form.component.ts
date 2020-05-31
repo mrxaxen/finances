e37786e75d3b6318523498a6018bdb4ccb79638c
Kovács Attila <ko.attila4@gmail.com>
2020-05-31 10:29:05 +0200

    implement ngModel into transaction form

--- frontend/finances-client/src/app/transaction-form/transaction-form.component.ts
+++ frontend/finances-client/src/app/transaction-form/transaction-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter  } from '@angular/core'; 
 import { Transaction } from '../transaction';
 
 @Component({
@@ -6,11 +6,27 @@ import { Transaction } from '../transaction';
   templateUrl: './transaction-form.component.html',
   styleUrls: ['./transaction-form.component.css']
 })
-export class TransactionFormComponent implements OnInit {
+export class TransactionFormComponent implements OnInit, OnChanges {
   @Input() transaction:Transaction 
   constructor() { }
 
+  model:Transaction
+  @Output() onSubmit = new EventEmitter<Transaction>();
+
   ngOnInit(): void {
+  }
+
+  //setting empty obj or the transaction object
+  ngOnChanges() {
+    this.model = Object.assign({}, this.transaction);
+  }
+
+  
+  submit(form) {
+    if (!form.valid) {
+      return;
+    }
+    this.onSubmit.emit(this.model);
   }
 
 }
