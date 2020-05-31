330cc970376d35359a77b88f62f32c1c162ccf18
Kovács Attila <ko.attila4@gmail.com>
2020-05-31 15:22:48 +0200

    updating login-fomr.component.ts

--- frontend/finances-client/src/app/login-form/login-form.component.ts
+++ frontend/finances-client/src/app/login-form/login-form.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-form',
+  selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  
+  message: string;
+  hidePassword = true;
 
-  constructor() { }
+  form = this.fb.group({
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required ]],
+  });
 
-  ngOnInit(): void {
+  get username() { return this.form.get('username'); }
+  get password() { return this.form.get('password'); }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
   }
 
+  async onSubmit() {
+    try {
+      this.message = null;
+      await this.authService.login(this.username.value, this.password.value);
+      if (this.authService.redirectUrl) {
+        this.router.navigate([this.authService.redirectUrl]);
+      } else {
+        this.router.navigate(['/']);
+      }
+    } catch (e) {
+      this.message = 'Cannot log in!';
+    }
+  }
 }
