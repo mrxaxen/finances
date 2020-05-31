bef6455ca7416ff84332028107087540a7cad320
Kovács Attila <ko.attila4@gmail.com>
2020-05-31 23:58:58 +0200

    added diagram

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 insert into user (username, password, enabled, role) values ('user', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER');
+insert into user (username, password, enabled, role) values ('admin', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_ADMIN');
 
-insert into transaction(title,change,user_id,creation_date) values('Penny',-2000,1,CURRENT_TIMESTAMP());
-insert into transaction(title,change,user_id,creation_date) values('Bills',-20000,1,CURRENT_TIMESTAMP());
+insert into transaction(title,change,user_id,creation_date) values('Penny',-4000,1,CURRENT_TIMESTAMP());
+insert into transaction(title,change,user_id,creation_date) values('Bills',-10000,1,CURRENT_TIMESTAMP());
 insert into transaction(title,change,user_id,creation_date) values('Salary',100000,1,CURRENT_TIMESTAMP());
+insert into transaction(title,change,user_id,creation_date) values('Transport',-3500,1,CURRENT_TIMESTAMP());
+
