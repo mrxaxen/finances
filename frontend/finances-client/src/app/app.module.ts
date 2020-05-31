7e803ef616fc3c086d3969c29ec3ca40a594e9f6
Kovács Attila <ko.attila4@gmail.com>
2020-05-31 08:49:53 +0200

    adding transaction-details component

--- frontend/finances-client/src/app/app.module.ts
+++ frontend/finances-client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 import { ViewReportComponent } from './view-report/view-report.component';
+import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ViewReportComponent } from './view-report/view-report.component';
     NavbarComponent,
     TransactionListComponent,
     AddTransactionComponent,
-    ViewReportComponent
+    ViewReportComponent,
+    TransactionDetailsComponent
   ],
   imports: [
     BrowserModule,
