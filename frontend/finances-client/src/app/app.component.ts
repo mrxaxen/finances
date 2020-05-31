aec77559e2913c699502a4cdbf3f0fdd49dc1416
Kovács Attila <ko.attila4@gmail.com>
2020-05-31 15:15:32 +0200

    adding logout to app.component.ts

--- frontend/finances-client/src/app/app.component.ts
+++ frontend/finances-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finances-client';
+
+  constructor(public authService: AuthService) { }
+
+  public logout(): void
+  {
+    this.authService.logout();
+  }
+
+  
 }
